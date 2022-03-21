6e9e43dc1daf5047401bf8c815722e486c3ca0c2
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-03-21 15:48:08 +0100

    Update myveryfirstapp.py

--- myveryfirstapp.py
+++ myveryfirstapp.py
@@ -5,7 +5,7 @@ title = st.text_input('Please insert a film title')
 st.write('The current film title is', title)
 
 genre = st.radio("What's your favourite movie genre",('COMEDY', 'DRAMA', 'DOCUMENTARY'))
-IF GENRE == 'COMEDY':
+if genre == 'COMEDY':
   st.write('You selected comedy.')
   else:
     st.write('You didnt')
