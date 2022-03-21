41d05cdd145f460e15e8503e5336245b884a9950
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-03-21 15:40:06 +0100

    Update myveryfirstapp.py

--- myveryfirstapp.py
+++ myveryfirstapp.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.header("Hello World!")
 st.text("from Brixen")
-title = st.text_input('Please insert a film title', '<enter film title here in English>')
+title = st.text_input('Please insert a film title')
 st.write('The current film title is', title)
