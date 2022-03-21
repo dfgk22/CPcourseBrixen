3adadf037ae88a3bce8b4ff16f7208ee8fb2f4c8
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-03-21 16:01:54 +0100

    Update myveryfirstapp.py

--- myveryfirstapp.py
+++ myveryfirstapp.py
@@ -1,6 +1,11 @@
 import streamlit as st
-genre = st.radio("What's your favorite movie genre",('Comedy', 'Drama', 'Documentary'))
-if genre == 'Comedy':
-     st.write('You selected comedy.')
-else:
-     st.write("You didn't select comedy.")
+import json, requests
+ 
+APIkey = '84b9d611cf03e3a6ae68ef145b1a6ffc'
+location = 'Trento'
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location +'&appid=' + APIkey + '&lang=it' + '&units=metric'
+response = requests.get(url) 
+weatherData = json.loads(response.text)
+
+st.text(weatherData['main']['temp_max']) 
