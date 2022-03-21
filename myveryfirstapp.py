10518931f877d346b0cd9a361ed0f922117efe19
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-03-21 16:14:38 +0100

    Update myveryfirstapp.py

--- myveryfirstapp.py
+++ myveryfirstapp.py
@@ -2,12 +2,12 @@ import streamlit as st
 import json, requests
  
 APIkey = '84b9d611cf03e3a6ae68ef145b1a6ffc'
-location = st.text_input('Select a city', '<Enter the city name here>')
+location = st.text_input('Please enter a city', '<Enter the city name here>')
 
 url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location +'&appid=' + APIkey + '&lang=it' + '&units=metric'
 response = requests.get(url) 
 weatherData = json.loads(response.text)
 
 st.header('This is the weather forecast for ' + location + ' for today')
-st.text('The minimun temperature for ' + location + 'is ' + str(weatherData['main']['temp_min']) + 'ºC')
-st.text('The maximun temperature for ' + location + 'is ' + str(weatherData['main']['temp_max']) + 'ºC')
+st.text('The minimun temperature for ' + location + ' will be ' + str(weatherData['main']['temp_min']) + 'ºC')
+st.text('The maximun temperature for ' + location + ' will be ' + str(weatherData['main']['temp_max']) + 'ºC')
