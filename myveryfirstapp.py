40590f032028d64a84ffbaebe7790760becaeef2
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-03-21 16:26:01 +0100

    Update myveryfirstapp.py

--- myveryfirstapp.py
+++ myveryfirstapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import json, requests
  
 APIkey = '84b9d611cf03e3a6ae68ef145b1a6ffc'
-location = st.radio("Please select a city",('Trento', 'Bolzano', 'Rome', 'Naples', 'Milan'))
+location = st.radio("Please select a city:",('Trento', 'Bolzano', 'Rome', 'Naples', 'Milan'))
 
 url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location +'&appid=' + APIkey + '&lang=it' + '&units=metric'
 response = requests.get(url) 
