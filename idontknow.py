d44aaa6bd1baf092fc474367cb585879e1f934a4
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-03-28 16:08:54 +0200

    Update idontknow.py

--- idontknow.py
+++ idontknow.py
@@ -2,7 +2,7 @@ import json, requests
 from pprint import pprint
 
 
-option = st.selectbox('How would you like to be contacted?', ('Email', 'Home phone', 'Mobile phone’))
+option = st.selectbox('How would you like to be contacted?', ('Email', 'Home phone', 'Mobile phone'))
 st.write('You selected:', option):
                                                                
                                                                
