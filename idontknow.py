6c1b84feeb07353835318155b3642fa3c823ffee
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-03-28 16:07:00 +0200

    Update idontknow.py

--- idontknow.py
+++ idontknow.py
@@ -1,3 +1,16 @@
+import json,requests
+from pprint import pprint
+
+keyword=input('plz give me a keyword ')
+url= 'https://api.datamuse.com/words?sl=' + keyword + '&max=10'
+
+
+response = requests.get(url)   
+
+dataFromDatamuse = json.loads(response.text) 
+
+pprint(dataFromDatamuse[0:4])#if you just want to see the first 9 results
+
 option = st.selectbox('How would you like to be contacted?', ('Email', 'Home phone', 'Mobile phone’))
 st.write('You selected:', option):
                                                                
