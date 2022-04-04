051d93b6635d8d834f29460b23b5976aab42cb18
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-04-04 16:24:23 +0200

    Update translatorapp.py

--- translatorapp.py
+++ translatorapp.py
@@ -5,6 +5,6 @@ translator = Translator()
 
 word = st.text_input('Give me a word or a phrase to translate') 
 
-abc = translator.translate(word, dest=it)
+abc = translator.translate(word, dest='it')
 
 st.write('The translation is', abc.text)
