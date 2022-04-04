a48a9647c7802b6270117898f03fa467badf9abf
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-04-04 16:10:55 +0200

    Update translatorapp.py

--- translatorapp.py
+++ translatorapp.py
@@ -1,12 +1,1 @@
 from googletrans import Translator
-
-translator = Translator()
-
-while True:
-  word = input('Give me a word or a phrase to translate, else just type nothing ') 
-  if word == 'nothing':
-    print('okay bye!')
-    break
-  destlang = input('Tell e a two letter code for the destination language like es or en: ')
-  abc = translator.translate(word, dest=destlang)
-  print('The translatoon is', abc.text)
