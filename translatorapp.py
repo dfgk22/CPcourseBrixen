57eb7c4ecc24c4fb3e70202a4d74fba46ff423ac
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-04-04 16:35:47 +0200

    Update translatorapp.py

--- translatorapp.py
+++ translatorapp.py
@@ -1,11 +1,10 @@
-from googletrans import Translator
 import streamlit as st
 
 translator = Translator()
 
 st.title('Translator')
 
-word = st.text_input('Give me a word or a phrase to translate into italian') 
+word = st.text_input('Give me a word or a phrase to translate into Italian') 
 
 abc = translator.translate(word, dest='it')
 
