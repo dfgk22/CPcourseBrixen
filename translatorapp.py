54ebe5b6971ad619935019062eb53d357ebbcdf4
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-04-04 16:19:36 +0200

    Update translatorapp.py

--- translatorapp.py
+++ translatorapp.py
@@ -1,1 +1,10 @@
 from googletrans import Translator
+import streamlit as st
+
+translator = Translator()
+
+word = st.text_input('Give me a word or a phrase to translate, else just type nothing ') 
+
+abc = translator.translate(word)
+
+st.write('The translatoon is', abc.text)
