585dbebf709ab4d0db271bc8a6fbacd707df5ee3
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2022-04-04 16:51:30 +0200

    Update translatorapp.py

--- translatorapp.py
+++ translatorapp.py
@@ -8,5 +8,6 @@ st.title('Translator')
 word = st.text_input('Give me a word or a phrase to translate into Italian', '') 
 
 if word != '':
-  abc = translator.translate(word, dest='it')
+  lang = st.text_input('Give me a target language', '') 
+  abc = translator.translate(word, dest='lang')
   st.write('The translation is', abc.text)
