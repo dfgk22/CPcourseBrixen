1097b8a4ab5c29e04fce3a1893bc07e61e801b2d
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2024-01-26 21:01:10 +0100

    Update myapp5.py

--- myapp5.py
+++ myapp5.py
@@ -33,42 +33,32 @@ if st.session_state.page == 0:
         if st.session_state.user_input:
             st.button("Check Answer", disabled=(st.session_state.page > 1))
 
-    if st.session_state.page == 1:
-        with placeholder.container():
-            st.image(Image.open(f"image/{st.session_state.rand_item}.jpg"), width=300)
-            st.write(f"You entered: {st.session_state.user_input}")
+    if st.session_state.user_input2:
+        if st.session_state.user_input2.lower() == str(st.session_state.rand_item):
+            st.write(f"Great! That is correct. The word is {st.session_state.rand_item}")
+            text_to_translate = str(st.session_state.rand_item)
+            translator = Translator(to_lang='it')
+            result = Translator.translate(translator, text_to_translate)
             st.write("\n")
+            st.write(f"The word '{st.session_state.rand_item}' translates to '{result}' in Italian.")
             st.write("\n")
+            st.write("Now you can keep practicing the pronunciation of this word...")
             tts = gTTS(text=st.session_state.rand_item, lang='en')
             tts.save('user.mp3')
             st.audio('user.mp3')
-            st.session_state.user_input2 = st.text_input("Enter the word and press Enter", key=2)
-            if st.session_state.user_input2:
-                if st.session_state.user_input2.lower() == str(st.session_state.rand_item):
-                    st.write(f"Great! That is correct. The word is {st.session_state.rand_item}")
-                    text_to_translate = str(st.session_state.rand_item)
-                    translator = Translator(to_lang='it')
-                    result = Translator.translate(translator, text_to_translate)
-                    st.write("\n")
-                    st.write(f"The word '{st.session_state.rand_item}' translates to '{result}' in Italian.")
-                    st.write("\n")
-                    st.write("Now you can keep practicing the pronunciation of this word...")
-                    tts = gTTS(text=st.session_state.rand_item, lang='en')
-                    tts.save('user.mp3')
-                    st.audio('user.mp3')
-                    st.button("Continue", on_click=restart, disabled=(st.session_state.page > 1))
-                else:
-                    text_to_translate = str(st.session_state.rand_item)
-                    translator = Translator(to_lang='it')
-                    result = Translator.translate(translator, text_to_translate)
-                    st.write(f"Sorry, but {st.session_state.user_input2} is not correct either. The correct word is {st.session_state.rand_item}.")
-                    st.write(f"'{st.session_state.rand_item}' translates to '{result}' in Italian.")
-                    st.write("Make sure to practice by pronouncing this word with the audio.")
-                    tts = gTTS(text=st.session_state.rand_item, lang='en')
-                    tts.save('user.mp3')
-                    st.audio('user.mp3')
+            st.button("Continue", on_click=restart, disabled=(st.session_state.page > 1))
+        else:
+            text_to_translate = str(st.session_state.rand_item)
+            translator = Translator(to_lang='it')
+            result = Translator.translate(translator, text_to_translate)
+            st.write(f"Sorry, but {st.session_state.user_input2} is not correct either. The correct word is {st.session_state.rand_item}.")
+            st.write(f"'{st.session_state.rand_item}' translates to '{result}' in Italian.")
+            st.write("Make sure to practice by pronouncing this word with the audio.")
+            tts = gTTS(text=st.session_state.rand_item, lang='en')
+            tts.save('user.mp3')
+            st.audio('user.mp3')
 
-    else:
-        with placeholder.container():
-            st.header("Well done! You completed this exercise. If you want to continue practicing, click on the NEW EXERCISE button")
-            st.button("NEW EXERCISE", on_click=restart)
+else:
+    with placeholder.container():
+        st.header("Well done! You completed this exercise. If you want to continue practicing, click on the NEW EXERCISE button")
+        st.button("NEW EXERCISE", on_click=restart)
