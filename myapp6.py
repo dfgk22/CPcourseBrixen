0e6c17a05a6cbd60aff22827f3a71d366f5ba113
dfgk22 <101569316+dfgk22@users.noreply.github.com>
2024-01-26 20:31:08 +0000

    Update myapp6.py

--- myapp6.py
+++ myapp6.py
@@ -31,15 +31,16 @@ if st.session_state.page == 0:
   with placeholder.container():
     st.title("The Ultimate IPA Quiz: Decoding English Words")
     st.write("\n")
-    st.text("Embark on a linguistic adventure with the immersive IPA (International Phonetic Alphabet) Quiz, designed to challenge and enhance your understanding of English pronunciation")
+    st.text("Embark on a linguistic adventure with the immersive IPA (International Phonetic Alphabet) Quiz")
+    st.text("designed to challenge and enhance your understanding of English pronunciation")
     st.write("\n")
-    st.header("Decipher the IPA symbol :uk: :uk: :uk:")
+    st.header(":us: :us: :us: Decipher the IPA symbol :uk: :uk: :uk:")
     st.write("\n")
     st.write("\n")
   picture = "image/" + st.session_state.rand_item + '.jpg'
   img = Image.open(picture)
   st.image(img, width=300)
-  st.session_state.user_input = st.text_input("Type in the English word you see in the IPA symbol", key=1)
+  st.session_state.user_input = st.text_input("Type in the English word you see in the IPA symbol, and then press continue", key=1)
   st.button("Continue",on_click=nextpage,disabled=(st.session_state.page > 1))
   
 
@@ -79,7 +80,7 @@ elif st.session_state.page == 1:
             text_to_translate = str(st.session_state.rand_item)
             translator = Translator(to_lang='it')
             result = Translator.translate(translator, text_to_translate)
-            st.write("Sorry, but ",st.session_state.user_input2,"is not correct either. The correct word is " ,st.session_state.rand_item,".")
+            st.write("Sorry, but ",st.session_state.user_input2," is not correct either. The correct word is ", st.session_state.rand_item,".")
             st.write("'",st.session_state.rand_item,"' translates to '",result,"' in italian.")
             st.write("Make sure to practice by pronouncing this word with the audio.")
             tts=gTTS(text= st.session_state.rand_item, lang='en')
@@ -87,7 +88,3 @@ elif st.session_state.page == 1:
             st.audio('user.mp3')
               
 
-else:
-  with placeholder.container():
-        st.header("Please restart the app.")
-        st.button("Restart", on_click=restart)
